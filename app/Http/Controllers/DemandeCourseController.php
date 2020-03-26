f44d2702302ff5a11a700a28fe41ef487708317a
eya tayari <eya.tayari@outlook.com>
2020-03-26 15:22:49 +0000

    Update DemandeCourseController.php

--- app/Http/Controllers/DemandeCourseController.php
+++ app/Http/Controllers/DemandeCourseController.php
@@ -80,7 +80,7 @@ class DemandeCourseController extends Controller
 
 
         $data->save();
-            return redirect("/home");
+            return redirect(route('home'));
       
        
       
@@ -88,48 +88,4 @@ class DemandeCourseController extends Controller
       
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-      
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-      
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-      
-    }
 }
